0a4b244c9deb74827f9fc4418e87eb251a6cc2ff
Gonçalo Teixeira <up201806562@fe.up.pt>
2021-03-13 13:24:25 +0000

    reading files on tests

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -1,52 +1,30 @@
-import static org.junit.Assert.*;
-import org.junit.After;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.util.Properties;
-import java.io.StringReader;
-
 import pt.up.fe.comp.TestUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
 public class ExampleTest {
+    public String parse(final String filename) throws IOException {
+        File file = new File("test/fixtures/public/" + filename);
+        FileInputStream fis = new FileInputStream(file);
+        byte[] data = new byte[(int) file.length()];
+        fis.read(data);
+        fis.close();
+
+        return new String(data, StandardCharsets.UTF_8);
+    }
 
 
     @Test
-    public void testExpression() {
-        String testImport = "import io;\n" +
-                "class WhileAndIF {\n" +
-                " \n" +
-                "    public static void main(String[] args){\n" +
-                "        int a;\n" +
-                "        int b;\n" +
-                "        int c;\n" +
-                "        int[] d;\n" +
-                "        a = 20;\n" +
-                "        b = 10;\n" +
-                "        d = new int[10];\n" +
-                "        if( a < b){\n" +
-                "            c  = a-1;\n" +
-                "        }else{\n" +
-                "            c = b-1;\n" +
-                "        }\n" +
-                "\n" +
-                "\n" +
-                "        while((0-1) < c){\n" +
-                "            d[c] = a-b; \n" +
-                "            c= c-1;\n" +
-                "            a= a-1;\n" +
-                "            b= b-1;\n" +
-                "        }\n" +
-                "        c=0;\n" +
-                "        while(c < d.length){\n" +
-                "            io.println(d[c]);\n" +
-                "            c= c+1;\n" +
-                "        }\n" +
-                "    }\n" +
-                "\n" +
-                "}";
+    public void testExpression() throws IOException {
+        String findMax = parse("FindMaximum.jmm");
 
-		assertEquals("Program", TestUtils.parse(testImport).getRootNode().getKind());
+		assertEquals("Program", TestUtils.parse(findMax).getRootNode().getKind());
 	}
 
 }
