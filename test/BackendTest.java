b5e3fb20dcca25f3180ebd2f0908a2f9088b74c0
Gonçalo Teixeira <up201806562@fe.up.pt>
2021-05-31 14:55:23 +0100

    added merge sort jmm

--- test/BackendTest.java
+++ test/BackendTest.java
@@ -108,4 +108,12 @@ public class BackendTest {
 
         String output = result.run("20");
     }
+
+    @Test
+    public void testMergeSort() {
+        JasminResult result = TestUtils.backend(SpecsIo.getResource("fixtures/public/MergeSort.jmm"));
+        TestUtils.noErrors(result.getReports());
+
+        String output = result.run("50");
+    }
 }
